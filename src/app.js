0312874d89018c81172aabc4e173a838d0ebb80f
Antonio D'Onchia <antonio.donchia@hotmail.com>
2019-06-29 11:49:38 +0200

    Setup for heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const request = require("request")
 
 const { geocode, forecast } = require("./utils")
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 // define pathe for Express config
 const publicDir = join(__dirname, "../public")
